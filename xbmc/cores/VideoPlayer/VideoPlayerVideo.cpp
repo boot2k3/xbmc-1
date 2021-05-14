458c9232d99293bb6d211e8b130a3d12c9789b6e
Dmitry_L <freebox_2002@mail.ru>
2021-05-14 23:49:35 +0300

    VideoPlayerVideo: Fix interlace/progressive info in PlayerDebug
    Now interlace flag checked only once on stream start or after some stream change.
    Contains revert for 2 commits:
    https://github.com/CoreELEC/xbmc/commit/e6f7a0d214f05dbf183fceaea662b851316635b0
    https://github.com/CoreELEC/xbmc/commit/a655c4fc29700d384cba9ce277a922d50517ad41

--- xbmc/cores/VideoPlayer/VideoPlayerVideo.cpp
+++ xbmc/cores/VideoPlayer/VideoPlayerVideo.cpp
@@ -18,6 +18,7 @@
 #include "settings/SettingsComponent.h"
 #include "utils/MathUtils.h"
 #include "utils/log.h"
+#include "utils/SysfsUtils.h"
 #include "windowing/GraphicContext.h"
 #include "windowing/WinSystem.h"
 
@@ -332,6 +333,9 @@ void CVideoPlayerVideo::Process()
   int iDropDirective;
   bool onlyPrioMsgs = false;
 
+  std::string vfmt;
+  int vfmtCheckCount = 0;
+
   m_videoStats.Start();
   m_droppingStats.Reset();
   m_iDroppedFrames = 0;
@@ -371,6 +375,8 @@ void CVideoPlayerVideo::Process()
           m_picture.videoBuffer)
       {
         m_outputSate = OutputPicture(&m_picture);
+        vfmtCheckCount = 2;
+        CLog::Log(LOGDEBUG, "CVideoPlayerVideo - OUTPUT_AGAIN - vfmt, interlace should be checked.");
         if (m_outputSate == OUTPUT_AGAIN)
         {
           onlyPrioMsgs = true;
@@ -441,6 +447,8 @@ void CVideoPlayerVideo::Process()
       m_renderManager.ShowVideo(true);
 
       CLog::Log(LOGDEBUG, "CVideoPlayerVideo - CDVDMsg::GENERAL_RESYNC(%f)", pts);
+      vfmtCheckCount = 2;
+      CLog::Log(LOGDEBUG, "CVideoPlayerVideo - vfmt, interlace should be checked.");
     }
     else if (pMsg->IsType(CDVDMsg::VIDEO_SET_ASPECT))
     {
@@ -601,6 +609,16 @@ void CVideoPlayerVideo::Process()
         if (ProcessDecoderOutput(frametime, pts))
         {
           onlyPrioMsgs = true;
+        }
+
+        if (vfmtCheckCount != 0)
+        {
+          if(!SysfsUtils::GetString("/sys/class/deinterlace/di0/frame_format", vfmt) && (vfmt.size() > 4))
+          {
+            m_processInfo.SetVideoInterlaced(vfmt.compare("progressive"));
+          }
+          vfmtCheckCount--;
+          CLog::Log(LOGDEBUG, "CVideoPlayerVideo - CDVDMsg::DEMUXER_PACKET - checking interlace vfmt: %s", vfmt);
         }
       }
       else
